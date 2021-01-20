e0e0aaa00f13cd051a768eeb98ad7c261bafef66
devolisto <devolisto@gmail.com>
2021-01-20 16:16:55 +0300

    did my best

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import shopFront from './images/shopFront.jpg'
+import Nezuko from './images/Nezuko.jpg'
+import Luffy from './images/luffy.jpg'
+import products from "./products";
 
 // const title = "Ecnious Shop"
 
 function App() {
+
+  const productList = products.map((product) => {
+      return (product.image, product.name, product.price)
+  })
   return (
   
       // <title>{title}</title>
       <div>
-        <h1 className="welcome">Welcome to Ecnious Shop</h1>
-        <h3 className="description">You're best destination for all anime stuff</h3>
-        <img className="image" src={shopFront} alt="shop front" ></img>
+        <div>
+          <h1 className="welcome">Welcome to Ecnious Shop</h1>
+          <h3 className="description">You're best destination for all anime stuff</h3>
+          <img className="image" src={shopFront} alt="shop front" ></img>
+        </div>
+
+        <div className="side">
+        <img className="figures" src={Nezuko} alt="Nezuko" />
+        <h4>Nezuko Action figure</h4>
+        <h4>250SR</h4>
+        </div>
+
+        <div className="side">
+        <img className="figures" src={Luffy} alt="Luffy" />
+        <h4>Luffy Action figure</h4>
+        <h4>350SR</h4>
+        </div>
+
       </div>
 
   );
@@ -20,7 +42,7 @@ function App() {
 export default App;
 
 
-{/* <div className="App">
+/* <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -35,4 +57,4 @@ export default App;
           Learn React
         </a>
       </header>
-    </div> */}
+    </div> */
---
+++ src/products.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+        name: "Nezuko Action figure",
+        price: "250SR",
+        image: <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.com%2FKotobukiya-Nezuko-Kamado-ARTFX-Statue%2Fdp%2FB08DQ8YDJK&psig=AOvVaw1-kn2fj0VA5O-CrMvlxEds&ust=1611232022057000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOCw6Y_Bqu4CFQAAAAAdAAAAABAG"></img>
+    },
+    {
+        name: "Luffy Action figure",
+        price: "350SR",
+        image: <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fanimemonks.com%2Fproducts%2Fone-piece-monkey-d-luffy-action-figure-model&psig=AOvVaw22x-GgNEZE2PN2q3aDH3GV&ust=1611232115649000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICR7r7Bqu4CFQAAAAAdAAAAABAD"></img>
+    }
+]
+
+export default products;
